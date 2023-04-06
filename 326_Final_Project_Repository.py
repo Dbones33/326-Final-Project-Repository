68c2168b40fc606194faa3c83c9e2d58d97282a6
David Kneebone <dkneebon@umd.edu>
2023-04-06 16:38:43 -0400

    cd second commit

---
+++ 326_Final_Project_Repository.py
@@ -0,0 +1,9 @@
+"""A program for playing the game, Ludo."""
+
+class Player:
+    """A player in the game, Ludo.
+    
+    Attributes:
+        name (str): the player's name.
+        other_players (set of Person): other players in the game.
+    """
