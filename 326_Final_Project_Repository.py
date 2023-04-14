515c4ab7d05d12b723621ea3a485388f3fe95cf4
Justin Gold <jgold123@terpmail.umd.edu>
2023-04-14 11:07:05 -0400

    pseudocode for the 'select_piece()' function

--- 326_Final_Project_Repository.py
+++ 326_Final_Project_Repository.py
@@ -7,6 +7,19 @@ class Player:
         name (str): the player's name.
         other_players (set of Person): other players in the game.
     """
+    
+def select_piece():
+    """Player selects which piece to move for their turn.
+    
+    Args:
+        player (str): the player's name
+        piece (str): the player's piece 
+        
+    Returns:
+        The piece that the player selected to play with for thier turn.
+    
+    """
+    
 def rotate():
     '''Moves player pieces around the game board.
     
