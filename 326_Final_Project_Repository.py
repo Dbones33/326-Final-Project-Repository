2d400ac28cc08fd2b3e3931d14b6210c13b761cb
David Kneebone <dkneebon@umd.edu>
2023-04-14 17:22:37 -0400

    Added psuedocode for a create_board method.

--- 326_Final_Project_Repository.py
+++ 326_Final_Project_Repository.py
@@ -7,3 +7,19 @@ class Player:
         name (str): the player's name.
         other_players (set of Person): other players in the game.
     """
+    
+def create_board(self):
+     """Creates five separate lists with string values that represents spaces
+     on the board. One list for the general board, and four others for each of
+     the color's separate finish lines."
+            
+        Side-effects:
+            Will create five separate lists and modify them to have string 
+            values for each space that the list should contain.      
+            """
+            
+
+  
+  
+    
+    
