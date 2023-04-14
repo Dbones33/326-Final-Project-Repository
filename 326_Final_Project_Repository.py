b433f1d1bde3a739029b15274f0961c0fb9a566c
SailorEmmitt <123580582+SailorEmmitt@users.noreply.github.com>
2023-04-13 20:20:31 -0400

    Update 326_Final_Project_Repository.py

--- 326_Final_Project_Repository.py
+++ 326_Final_Project_Repository.py
@@ -7,3 +7,15 @@ class Player:
         name (str): the player's name.
         other_players (set of Person): other players in the game.
     """
+def rotate():
+    '''Moves player pieces around the game board.
+    
+    Args:
+        name(str): player's name
+        piece(str): player's piece
+        location(int): current piece location on the board
+        
+    Returns:
+        A player piece's location after it moves 
+    '''
+        
