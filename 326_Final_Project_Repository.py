32d225437681d92b0abcdfbb836453dc244bddf6
SailorEmmitt <123580582+SailorEmmitt@users.noreply.github.com>
2023-05-02 11:34:06 -0400

    Update 326_Final_Project_Repository.py

--- 326_Final_Project_Repository.py
+++ 326_Final_Project_Repository.py
@@ -8,7 +8,9 @@ class Player:
         other_players (set of Person): other players in the game.
     """
     
-def create_board(self):
+class LudoBoard:
+    
+    def create_board(self):
     """Creates five separate lists with string values that represents spaces
     on the board. One list for the general board, and four others for each of
     the color's separate finish lines."
@@ -17,66 +19,52 @@ def create_board(self):
             Will create five separate lists and modify them to have string 
             values for each space that the list should contain.      
             """
-    board = []
-    i = 1
-    while i <= 52:
+        board = []
+        i = 1
+        while i <= 52:
         board.append(f"space{i}")
         i += 1
 
-    yellow_finish_line = []
-    count = 1
-    while count <= 6:
-        yellow_finish_line.append(f"space{i}")
-        count += 1
-    blue_finish_line = []
-    count = 1
-    while count <= 6:
-        blue_finish_line.append(f"space{i}")
-        count += 1
-    orange_finish_line = []
-    count = 1
-    while count <= 6:
-        orange_finish_line.append(f"space{i}")
-        count += 1
-    green_finish_line = []
-    count = 1
-    while count <= 6:
-        green_finish_line.append(f"space{i}")
-        count += 1   
+        yellow_finish_line = []
+        count = 1
+        while count <= 6:
+            yellow_finish_line.append(f"space{i}")
+            count += 1
+        blue_finish_line = []
+        count = 1
+        while count <= 6:
+            blue_finish_line.append(f"space{i}")
+            count += 1
+        orange_finish_line = []
+        count = 1
+        while count <= 6:
+            orange_finish_line.append(f"space{i}")
+            count += 1
+        green_finish_line = []
+        count = 1
+        while count <= 6:
+            green_finish_line.append(f"space{i}")
+            count += 1   
                
-class Board:
+     def roll_dice(self):
+            return random.randint(1, 6)
 
-    def __init__(self, size):
-        self.size = size
-        self.board = [[None] * size for _ in range(size)]
+        def move_piece(self, player, piece):
+            roll = self.roll_dice()
+            new_position = self.players[player][piece] + roll
 
-    def place_piece(self, piece, x, y):
-        if x < self.size and y < self.size:
-            self.board[y][x] = piece
-        else:
-            raise ValueError("Invalid position entered.")
+            if new_position <= 57:
+                self.players[player][piece] = new_position
 
-    def move_piece(self, piece, x, y):
-        if x < self.size and y < self.size:
-            piece_x, piece_y = (-1, -1)
+                if new_position == 57:
+                    self.finished_pieces[player] += 1
+                    if self.finished_pieces[player] == 4:
+                        self.winner = player
 
-            for i in range(self.size):
-                for j in range(self.size):
-                    if self.board[j][i] == piece:
-                        piece_x, piece_y = (i, j)
-                        break
-
-            if piece_x != -1 and piece_y != -1:
-                self.board[piece_y][piece_x] = None
-                self.board[y][x] = piece
-            else:
-                raise ValueError("Piece not found on board.")
-        else:
-            raise ValueError("Invalid position entered.")
-
-    def print_board(self):
-        for row in self.board:
-            print(row)
+        def play_game(self):
+            while not self.winner:
+                self.play_round()
+             print(f"The winner is {self.winner}, congratulations!")
 
 
 class Piece:
@@ -126,22 +114,7 @@ class Select_Piece(Piece):
         return f"{self.player} has selected {self.piece}"
         
         
-        
-        
-        
-    
-def rotate():
-    '''Moves player pieces around the game board.
-    
-    Args:
-        name(str): player's name
-        piece(str): player's piece
-        location(int): current piece location on the board
-        
-    Returns:
-        A player piece's location after it moves 
-    '''
-    # My implementation of rotate
+      
     
 # Someone take over
 def restart():
